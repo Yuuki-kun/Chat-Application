8fc0b2f71426cf9950592662a678d56b7c4955cc
ManhHuynhLab <huynhtran3710@gmail.com>
2023-06-23 14:25:40 +0700

    DataDriver

--- AppChat/src/application/models/DataDriver.java
+++ AppChat/src/application/models/DataDriver.java
@@ -7,20 +7,50 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DataDriver {
-	
+
 	private Connection conn;
 	private PreparedStatement statement;
 	private ResultSet resultSet;
-	
-	public DataDriver() {
-		//connect sql
-		//conn = connect to sql
+
+	private Connection connectToSQL() {
+		Connection connection = null;
+		try {
+			
+			String url = "jdbc:sqlserver://localhost:1433;databaseName = "; //Database name
+			String username = ""; //UseName
+			String password = ""; //Password
+
+			connection = DriverManager.getConnection(url, username, password);
+
+			System.out.println("Connected to SQL Server");
+		} catch (SQLException e) {
+			System.out.println("Failed to connect to SQL Server");
+			e.printStackTrace();
+		}
+		return connection;
 	}
-	
+
+	public DataDriver() {
+		// connect sql
+//		conn = connect to sql;
+		conn = connectToSQL();
+	}
+
 	public ResultSet getExistUserResultSet(String username, String password) {
-		
-		//use PrepareStatement execute query
-	    		
+
+		// use PrepareStatement execute query
+
+		try {
+			String query = "SELECT * FROM users WHERE username = ? AND password = ?";
+			statement = conn.prepareStatement(query);
+			statement.setString(1, username);
+			statement.setString(2, password);
+
+			resultSet = statement.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 		return resultSet;
 	}
 }
