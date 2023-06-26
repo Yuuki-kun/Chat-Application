eb20fa8939f56c206a1b3fb5646700c97c713e57
Yuuki-kun <tongcongminhart@gmail.com>
2023-06-26 17:34:26 +0700

    clean

--- AppChat/src/application/models/ClientModel.java
+++ AppChat/src/application/models/ClientModel.java
@@ -1,31 +1,22 @@
 package application.models;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import accounttype.AccountType;
 import application.views.ViewFactory;
 import request.LoginRequest;
 import request.Request;
 import request.RequestType;
-import server.model.DataDriver;
 
 public class ClientModel {
 	
 	private static ClientModel model;
 	private final ViewFactory viewFactory;
 	private AccountType logginAccountType = AccountType.CLIENT;
-	private DataDriver datadriver;
 	private boolean loginSuccessfully = false;
 	private Client client;
 	
 	private ClientModel() {
 		this.viewFactory = new ViewFactory(logginAccountType);
 		this.client = new Client("localhost", 1234);
-	}
-	
-	public void connectSQL(String dbname, String usrname, String pwd) {
-		datadriver = new DataDriver(dbname,usrname,pwd);
 	}
 	
 	public static synchronized ClientModel getInstance() {
@@ -37,28 +28,7 @@ public class ClientModel {
 	public ViewFactory getViewFactory() {
 		return viewFactory;
 	}
-	
-	public void evaluateLoginType(String username, String password) {
-		ResultSet resultSet = datadriver.getExistUserResultSet(username, password);
-		try {
-			if(resultSet.next()) {
-				if(resultSet.getInt("type")==1) {
-					this.logginAccountType=AccountType.CLIENT;
-					System.out.println("CLIENT");
-				}else if(resultSet.getInt("type")==2){
-					System.out.println("ADIN");
-					this.logginAccountType=AccountType.ADMIN;
-				}else {
-					this.loginSuccessfully = false;
-				}
-				this.loginSuccessfully = true;
 
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
 
 	public void connectServer(String servername, int PORT) {
 		if(!servername.equals("") && servername!=null)
