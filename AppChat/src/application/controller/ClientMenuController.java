e06a2198fe2f8ac3cde0069508d09200b03c2939
Yuuki-kun <tongcongminhart@gmail.com>
2023-07-06 12:31:36 +0700

    those file will be removed

--- AppChat/src/application/controller/ClientMenuController.java
+++ AppChat/src/application/controller/ClientMenuController.java
@@ -1,3 +1,8 @@
+
+
+//BO CLASS NAY VA FILE CLIENT MENU - GOP VAO CLIENT
+
+
 package application.controller;
 
 import java.net.URL;
@@ -8,16 +13,33 @@ import com.jfoenix.controls.JFXButton;
 import application.models.ClientModel;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+
+
 
 public class ClientMenuController implements Initializable{
+
+    @FXML
+    private Label clientName;
+
+    @FXML
+    private JFXButton friendBook_btn;
+
     @FXML
     private JFXButton group_btn;
+
+    @FXML
+    private JFXButton logout_btn;
 
     @FXML
     private JFXButton message_btn;
 
     @FXML
-    private JFXButton friendBook_btn;
+    private JFXButton profile_btn;
+
+    @FXML
+    private JFXButton setting_btn;
     
     public void showMessageView() {
     		ClientModel.getInstance().getViewFactory().showMessageView();
@@ -29,8 +51,13 @@ public class ClientMenuController implements Initializable{
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
+				
 		message_btn.setOnAction(event -> showMessageView());
 		friendBook_btn.setOnAction(event -> showFriendBookView());
+	}
+	
+	public void setNameLabel(String name) {
+		this.clientName.setText(name);
 	}
     
 }
